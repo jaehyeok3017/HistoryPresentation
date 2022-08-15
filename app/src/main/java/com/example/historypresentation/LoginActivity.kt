07294e41adedbae2783b9fce2c15032bb6b861db
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-08-16 00:50:03 +0900

    Feat : `LoginActivity`, `RegisterActivity`, `SplashActivity`
    - Login, Register 기능 추가
    - Manifest 수정
    - SplashActivity 수정

---
+++ app/src/main/java/com/example/historypresentation/LoginActivity.kt
@@ -0,0 +1,71 @@
+package com.example.historypresentation
+
+import android.content.ContentValues
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.historypresentation.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    var auth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+        binding.loginbtn.setOnClickListener {
+            when(loginCompleteCheck(binding.email.text, binding.passwd.text)){
+                true -> signIn()
+                false -> Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.registerbtn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+
+    override fun onStart(){
+        super.onStart()
+        moveMainPage(auth?.currentUser)
+    }
+
+    private fun loginCompleteCheck(emailText: Editable, passwdText: Editable): Boolean {
+        val emailLength = emailText.length
+        val passwdLength = passwdText.length
+
+        return emailLength != 0 && passwdLength != 0
+    }
+
+    private fun signIn() {
+        auth?.signInWithEmailAndPassword(
+            binding.email.text.toString(),
+            binding.passwd.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(ContentValues.TAG, "signInWithEmail:success")
+                    moveMainPage(task.result?.user)
+                } else {
+                    Log.d(ContentValues.TAG, "failed")
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun moveMainPage(user: FirebaseUser?){
+        if(user != null) {
+            Toast.makeText(this, "로그인이 완료되었습니다!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/historypresentation/RegisterActivity.kt
@@ -0,0 +1,126 @@
+package com.example.historypresentation
+
+import android.content.ContentValues
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.historypresentation.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.util.regex.Pattern
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    var auth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+        binding.registercomplete.setOnClickListener {
+            binding.emailCheckText.visibility = View.INVISIBLE
+            binding.passwdCheckText.visibility = View.INVISIBLE
+            binding.repeatPasswdCheckText.visibility = View.INVISIBLE
+
+            val checkEmailResult = checkEmail()
+            val checkPasswdResult = checkPasswd()
+            val checkRepeatPasswdResult = checkRepeatPasswd()
+
+            if(checkEmailResult && checkPasswdResult && checkRepeatPasswdResult){
+                createUser()
+            }
+
+            else{
+                Toast.makeText(this, "각 형식을 확인해주세요", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkEmail(): Boolean {
+        val email = binding.registeremail.text.toString()
+        val emailFormatCheck = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val p = Pattern.matches(emailFormatCheck, email)
+
+        return when(p){
+            true -> true
+            false -> {
+                binding.emailCheckText.visibility = View.VISIBLE
+                false
+            }
+        }
+    }
+
+    private fun checkPasswd(): Boolean {
+        val passwd = binding.registerpasswd.text.toString()
+        val passwdFormatCheck = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\$@\$!%*#?&])[A-Za-z[0-9]\$@\$!%*#?&]{8,20}\$"
+        val p = Pattern.matches(passwdFormatCheck, passwd)
+
+        return when(p){
+            true -> true
+            false -> {
+                binding.passwdCheckText.visibility = View.VISIBLE
+                false
+            }
+        }
+    }
+
+    private fun checkRepeatPasswd(): Boolean {
+        val passwd = binding.registerpasswd.text.toString()
+        val repeatPasswd = binding.registerRepeatPasswd.text.toString()
+        val p = passwd == repeatPasswd
+
+        when(p){
+            true -> return true
+            false -> {
+                binding.repeatPasswdCheckText.visibility = View.VISIBLE
+                return false
+            }
+        }
+    }
+
+    private fun createUser(){
+        auth?.createUserWithEmailAndPassword(
+            binding.registeremail.text.toString(),
+            binding.registerpasswd.text.toString()
+        )
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(ContentValues.TAG, "createUserWithEmail:success")
+                    moveLoginPage(task.result?.user)
+                } else if(task.exception?.message.isNullOrEmpty()){
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    signIn()
+                }
+            }
+    }
+
+    private fun signIn() {
+        auth?.signInWithEmailAndPassword(
+            binding.registeremail.text.toString(),
+            binding.registerpasswd.text.toString()
+        )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d(ContentValues.TAG, "signInWithEmail:success")
+                    moveLoginPage(task.result?.user)
+                } else {
+                    Log.d(ContentValues.TAG, "failed")
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun moveLoginPage(user: FirebaseUser?){
+        if(user != null) {
+            Toast.makeText(this, "회원가입이 완료되었습니다!", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
