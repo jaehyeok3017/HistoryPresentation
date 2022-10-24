efc2934f43a8910da2beefa9d3a0e6ca5a8a21a5
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-10-24 22:40:11 +0900

    Fix : `LoginActivity`
    - LoginActivity 데이터 받아오는 과정 일부 수정

--- app/src/main/java/com/example/historypresentation/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/historypresentation/ui/login/LoginViewModel.kt
@@ -1,9 +1,13 @@
 package com.example.historypresentation.ui.login
 
+import android.content.ContentValues
+import android.util.Log
 import android.widget.Toast
+import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bumptech.glide.load.engine.Resource
 import com.example.historypresentation.data.repository.LoginRepository
 import com.google.firebase.auth.FirebaseAuth
 
@@ -12,22 +16,26 @@ class LoginViewModel : ViewModel() {
 
     private var auth: FirebaseAuth? = null
     val status = MutableLiveData<Boolean>(false)
-    val failReason = MutableLiveData<String>()
 
-    private val _idData = MutableLiveData<String>()
-    val idData : LiveData<String> get() = _idData
-
-    private val _pwData = MutableLiveData<String>()
-    val pwData : LiveData<String> get() = _pwData
+    val idData = MutableLiveData("")
+    val pwData = MutableLiveData("")
 
     fun login(){
-        when(loginCompleteCheck(idData.toString(), pwData.toString())){
+        Log.d(ContentValues.TAG, "id : ${idData.value.toString()} , pw : ${pwData.value.toString()}")
+        when(loginCompleteCheck(idData.value.toString(), pwData.value.toString())){
             false -> {
+                Log.d(ContentValues.TAG, "Complete Check 완료")
                 auth = FirebaseAuth.getInstance()
 
-                auth?.signInWithEmailAndPassword(idData.toString(), pwData.toString())
+                auth?.signInWithEmailAndPassword(idData.value.toString(), idData.value.toString())
                     ?.addOnCompleteListener { task ->
-                        status.value = task.isSuccessful
+                        if (task.isSuccessful) {
+                            Log.d(ContentValues.TAG, "Login 완료")
+                            status.value = true
+                        }
+                        else{
+                            status.value = false
+                        }
                     }
             }
 
@@ -36,6 +44,6 @@ class LoginViewModel : ViewModel() {
     }
 
     private fun loginCompleteCheck(email: String, password: String): Boolean {
-        return email.isBlank() && password.isBlank()
+        return email.isBlank() || password.isBlank()
     }
 }
