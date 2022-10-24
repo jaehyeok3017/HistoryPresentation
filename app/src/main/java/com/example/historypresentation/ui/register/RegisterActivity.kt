b1b8e08f0845674a3db4aa14ba655430dae3d4bc
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-10-25 00:25:36 +0900

    Fix : `RegisterActivity.kt` User Collection
    - user collection 제대로 설정되도록 수정

--- app/src/main/java/com/example/historypresentation/ui/register/RegisterActivity.kt
+++ app/src/main/java/com/example/historypresentation/ui/register/RegisterActivity.kt
@@ -8,17 +8,16 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.historypresentation.databinding.ActivityRegisterBinding
-import com.example.historypresentation.ui.login.LoginActivity
+import com.example.historypresentation.ui.main.MainActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
     var auth: FirebaseAuth? = null
-    private lateinit var db : FirebaseFirestore
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,11 +37,9 @@ class RegisterActivity : AppCompatActivity() {
             val checkPasswdResult = checkPasswd()
             val checkRepeatPasswdResult = checkRepeatPasswd()
 
-            if(checkEmailResult && checkPasswdResult && checkRepeatPasswdResult){
+            if (checkEmailResult && checkPasswdResult && checkRepeatPasswdResult) {
                 createUser()
-            }
-
-            else{
+            } else {
                 Toast.makeText(this, "각 형식을 확인해주세요", Toast.LENGTH_SHORT).show()
             }
         }
@@ -50,10 +47,11 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun checkEmail(): Boolean {
         val email = binding.registeremail.text.toString()
-        val emailFormatCheck = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val emailFormatCheck =
+            "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
         val p = Pattern.matches(emailFormatCheck, email)
 
-        return when(p){
+        return when (p) {
             true -> true
             false -> {
                 binding.emailCheckText.visibility = View.VISIBLE
@@ -64,10 +62,11 @@ class RegisterActivity : AppCompatActivity() {
 
     private fun checkPasswd(): Boolean {
         val passwd = binding.registerpasswd.text.toString()
-        val passwdFormatCheck = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\$@\$!%*#?&])[A-Za-z[0-9]\$@\$!%*#?&]{8,20}\$"
+        val passwdFormatCheck =
+            "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[\$@\$!%*#?&])[A-Za-z[0-9]\$@\$!%*#?&]{8,20}\$"
         val p = Pattern.matches(passwdFormatCheck, passwd)
 
-        return when(p){
+        return when (p) {
             true -> true
             false -> {
                 binding.passwdCheckText.visibility = View.VISIBLE
@@ -81,7 +80,7 @@ class RegisterActivity : AppCompatActivity() {
         val repeatPasswd = binding.registerRepeatPasswd.text.toString()
         val p = passwd == repeatPasswd
 
-        when(p){
+        when (p) {
             true -> return true
             false -> {
                 binding.repeatPasswdCheckText.visibility = View.VISIBLE
@@ -90,30 +89,32 @@ class RegisterActivity : AppCompatActivity() {
         }
     }
 
-    private fun createUser(){
+    private fun createUser() {
+        val email = binding.registeremail.text.toString()
+        val pw = binding.registerpasswd.text.toString()
+
         auth?.createUserWithEmailAndPassword(
-            binding.registeremail.text.toString(),
-            binding.registerpasswd.text.toString()
+            email, pw
         )
             ?.addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     val user = hashMapOf(
-                        "user" to binding.registeremail.toString(),
+                        "user" to email,
                         "testMaxScore" to 0,
                         "testRecentScore" to 0
                     )
 
                     db.collection("users")
-                        .document(binding.registeremail.toString())
+                        .document(email)
                         .set(user)
                         .addOnSuccessListener { documentReference ->
                             Log.d(ContentValues.TAG, "유저 정보 업로드 및 회원가입에 성공하였습니다.")
-                            moveLoginPage(task.result?.user)
+                            moveMainPage(task.result?.user)
                         }
                         .addOnFailureListener { e ->
                             Toast.makeText(this, "유저 정보 업로드에 실패했습니다.", Toast.LENGTH_SHORT).show()
                         }
-                } else if(task.exception?.message.isNullOrEmpty()){
+                } else if (task.exception?.message.isNullOrEmpty()) {
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
                 } else {
                     signIn()
@@ -129,7 +130,7 @@ class RegisterActivity : AppCompatActivity() {
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     Log.d(ContentValues.TAG, "signInWithEmail:success")
-                    moveLoginPage(task.result?.user)
+                    moveMainPage(task.result?.user)
                 } else {
                     Log.d(ContentValues.TAG, "failed")
                     Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show()
@@ -137,10 +138,10 @@ class RegisterActivity : AppCompatActivity() {
             }
     }
 
-    private fun moveLoginPage(user: FirebaseUser?){
-        if(user != null) {
+    private fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
             Toast.makeText(this, "회원가입이 완료되었습니다!", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, LoginActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
