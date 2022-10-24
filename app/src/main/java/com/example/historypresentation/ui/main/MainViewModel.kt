c92135d3cd371f316412e201445a566bc84f4650
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-10-24 18:05:07 +0900

    Fix : `MainViewModel`, `RegisterViewModel` DELETE
    - 메인 -> 뷰모델 사용 필요성 없어보여 삭제
    - 회원가입 -> 추후 구현 예정으로 삭제

--- app/src/main/java/com/example/historypresentation/ui/main/MainViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.historypresentation.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-}
--- app/src/main/java/com/example/historypresentation/ui/register/RegisterViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.historypresentation.ui.register
-
-import androidx.lifecycle.ViewModel
-
-class RegisterViewModel : ViewModel() {
-}
