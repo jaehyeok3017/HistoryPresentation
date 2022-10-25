d5fe5eb38cb52d87cbec855ca174f948e008ee8b
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-10-25 22:16:00 +0900

    Feat : `StudyFragment`
    - Study 부분 Fragment
    - image 불러오는 부분 문제 발생

--- app/src/main/java/com/example/historypresentation/ui/main/fragment/StudyFragment.kt
+++ app/src/main/java/com/example/historypresentation/ui/main/fragment/StudyFragment.kt
@@ -1,10 +1,35 @@
 package com.example.historypresentation.ui.main.fragment
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.BitmapDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.historypresentation.R
 import com.example.historypresentation.databinding.FragmentMainBinding
+import com.example.historypresentation.databinding.FragmentStudyBinding
 import com.example.historypresentation.ui.base.BaseFragment
 
-class StudyFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
+class StudyFragment : BaseFragment<FragmentStudyBinding>(R.layout.fragment_study) {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_study, container, false)
+
+        binding.scrollHorizontal.isHorizontalScrollBarEnabled = true
+        binding.scrollHorizontal.isVerticalScrollBarEnabled = true
+
+        binding.yearChange.setOnClickListener {
+            binding.yearImage.setImageResource(R.drawable.history2)
+        }
+
+        return binding.root
+    }
+
     override fun init() {
 
     }
