eb6ddd17cacdbaf1209d70c7bba70145bf6cbf89
최재혁 <66003250+jaehyeok3017@users.noreply.github.com>
2022-10-27 16:29:41 +0900

    Feat : `MainActivity` Bottom Navi Home Delete
    - Home 필요없어서 삭제

--- app/src/main/java/com/example/historypresentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/historypresentation/ui/main/MainActivity.kt
@@ -1,16 +1,9 @@
 package com.example.historypresentation.ui.main
 
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import com.example.historypresentation.R
-import com.example.historypresentation.databinding.ActivityLoginBinding
 import com.example.historypresentation.databinding.ActivityMainBinding
-import com.example.historypresentation.ui.base.BaseActivity
-import com.example.historypresentation.ui.main.fragment.MainFragment
 import com.example.historypresentation.ui.main.fragment.QuizFragment
 import com.example.historypresentation.ui.main.fragment.StudyFragment
 
@@ -27,9 +20,6 @@ class MainActivity : AppCompatActivity(){
 
         binding.bottomMenu.setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.nav_home -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom_main, MainFragment()).commit()
-                }
                 R.id.nav_study -> {
                     supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom_study, StudyFragment()).commit()
                 }
